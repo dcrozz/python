1242543820f9a2d4b61647f28032a8a564db09f1
ukeyim <ukeyim@gmail.com>
2016-04-17 13:40:48 +0800

    cleanup the content

--- re_test.py
+++
@@ -1,7 +0,0 @@
-emphasis_pattern = re.compile(r'''
-	\*	#Beginning emphasis tag -- an asterisk
-	(	#Begin group for capturing phrse
-	[^\*]+ # Capture anything except astericks
-	)	#End group
-	\*  # Ending emphasis tag
-'''), re.VERBOSE
